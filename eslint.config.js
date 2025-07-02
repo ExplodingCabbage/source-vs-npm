89411e807188b131157893a4b37ef246bc0351b3
Mark Amery <markrobertamery@gmail.com>
2025-07-02 10:57:59 +0100

    Fix errors about lines like `.filter(([_, count]) => count)`

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,23 @@ import globals from "globals";
 import json from "@eslint/json";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    rules: {
+      "no-unused-vars": ["error", { destructuredArrayIgnorePattern: "^_" }],
+    },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    files: ["**/*.json"],
+    plugins: { json },
+    language: "json/json",
+    extends: ["json/recommended"],
+  },
 ]);
