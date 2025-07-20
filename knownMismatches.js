d43714624a95faf9a5ce4c92cee050500c293f3d
Mark Amery <markrobertamery@gmail.com>
2025-07-20 15:37:10 +0100

    Approve resolve mismatch. 81/8/11

--- knownMismatches.js
+++ knownMismatches.js
@@ -2,4 +2,12 @@ export default {
   // Published version contains a licensing comment and some sponsorship info
   // in package.json that is missing from the source. Inconsequential.
   "safe-buffer": ["5.2.1"],
+
+  // resolve v1 ships with a "core.json" file that identifies which packages are
+  // "core modules" of Node (to be removed in v2 in favour of
+  // https://github.com/inspect-js/is-core-module). In v1.22.10, one version
+  // range in core.json was updated to match the change from
+  // https://github.com/inspect-js/is-core-module/commit/aafb7cae0976ecfb156bc563dde57ca8fd838d0c
+  // without that change being committed to source control. Innocuous.
+  resolve: ["1.22.10"],
 };
